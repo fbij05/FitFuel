c0bd648a9978b1ced4200404776bdbfb92383f24
Fahad Albeaiji <albeaijif@gmail.com>
2026-03-13 07:10:29 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// open and close the cart //
+var cart =document.querySelector('.cart');
+
+function open_cart(){
+    cart.classList.add("active")
+}
+function close_cart(){
+    cart.classList.remove("active")
+}
