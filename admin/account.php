0a2abb3ae7461cda97a1ed4cead6fdb4d538fd9c
Saeed Alahmari <258758218+sqahm@users.noreply.github.com>
2026-04-15 11:37:26 +0300

    admin panel skeleton

--- admin/account.php
+++ admin/account.php
@@ -18,9 +18,39 @@ if (!isset($_SESSION["user_id"])) {
     <title>FitFuel - Admin Panel</title>
 </head>
 <body>
-    <header></header>
+    <header>
+        <img src="../img/logo.png" alt="fitfuel logo" height="60">
+        <span id="corner-details">
+            <span id="logged-name"><?php echo $_SESSION["username"]?></span>
+            <button id="logout">Logout</button>
+        </span>
+    </header>
     <main>
-
+        <nav id="sidebar">
+            <ul>
+                <li><a href="account.php?show=items">Items</a></li>
+                <li><a href="account.php?show=account">Account</a></li>
+                <li><a href="account.php?show=contact">Contact</a></li>
+            </ul>
+        </nav>
+        <section id="content">
+            <?php
+                if(isset($_GET['show'])) {
+                    switch ($_GET['show']) {
+                        case 'items':
+                            include "tools/product_list.php";
+                            break;
+                        case 'account':
+                            include "tools/account_details.php";
+                            break;
+                        case 'contact':
+                            include "includes/contact.html";
+                        default:
+                            break;
+                    }
+                }
+            ?>
+        </section>
     </main>
 </body>
 </html>
---
+++ admin/tools/account_details.php
@@ -0,0 +1,5 @@
+<?php
+
+?>
+
+account details
