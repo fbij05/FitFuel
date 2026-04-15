87cbef1b40b1709b4efc0d56667a5c6381f645d5
Saeed Alahmari <258758218+sqahm@users.noreply.github.com>
2026-04-15 17:17:30 +0300

    skeleton account_details.php page

--- admin/account.php
+++ admin/account.php
@@ -21,7 +21,7 @@ if (!isset($_SESSION["user_id"])) {
     <header>
         <img src="../img/logo.png" alt="fitfuel logo" height="60">
         <span id="corner-details">
-            <span id="logged-name">Welcome, <?php echo $_SESSION["username"]?></span>
+            <span id="logged-name">Welcome, <?php echo $_SESSION["fullname"]?></span>
             <form method="post" action="logout.php" id="logout-form">
                 <button type="submit" id="logout">Logout</button>
             </form>
