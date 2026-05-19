c7b1d746052c46fa9269bb82973ecd5793dde77c
Saeed Alahmari <258758218+sqahm@users.noreply.github.com>
2026-05-19 23:15:13 +0300

    hide cart in checkout screen

--- checkout.php
+++ checkout.php
@@ -8,6 +8,10 @@
     <link rel="stylesheet" href="css/style.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+    <style>
+        .cart_header {display: none !important;}
+    </style>
+
 </head>
 
 <body>
